816b13825b6955ed1719cd77f48351ceae2ca96c
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2022-11-09 09:51:32 +0530

    Functions to carry basic mathematical operations

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 "use strict";
+
+function add(a, b)
+return a + b;
+
+function subtract(a, b)
+return a - b;
+
+function divide(a, b)
+return a / b;
+
+function multiply(a, b)
+return a * b;
+
+let a = 10, b = 20;
+
