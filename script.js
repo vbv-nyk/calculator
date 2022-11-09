681d9a2c1e2083c5645d7cc151cf6ffb2c013c36
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2022-11-09 12:59:51 +0530

    Added functionality to make calculations and display the answer

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 "use strict";
+const displayCurrent = document.querySelector(".current");
+const displayPrevious = document.querySelector(".previous");
+const displayAnswer = document.querySelector(".ans");
+let operator = false;
+let firstTime = true;
+let a = null;
+let opr = "";
+let b = null;
 
 function calculate(a, opr, b) {
     a = Number(a);
@@ -11,17 +19,40 @@ function calculate(a, opr, b) {
         case '*':
             return a * b;
         case '/':
-            return a / b;
+            if (b == 0)
+                alert("You can't divide a number by 0 lmao");
+            else
+                return a / b;
+        case '^':
+            return a ** b;
     }
 
 }
 
-function operator() {
-    let a = prompt("Enter number 1");
-    let b = prompt("Enter number 2");
-    let opr = prompt("Enter an operator");
-    console.log(calculate(a, opr, b));
-}
-
-// operator();
+window.addEventListener("keydown", (e) => {
+    const button = document.querySelector(`button[data-key="${e.key}"]`);
+    if (button) {
+        if (button.className == 'btn--num') {
+            if (displayCurrent.textContent == "0") {
+                displayCurrent.textContent = button.textContent;
+            } else {
+                displayCurrent.textContent += button.textContent;
+            }
+            if (a && opr) {
+                b = displayCurrent.textContent;
+                displayAnswer.textContent = calculate(a, opr, b);
+            }
+            operator = true;
+        } else if (button.className == "btn--opr" && operator) {
+            if (firstTime) {
+                a = displayCurrent.textContent;
+                firstTime = false;
+            } else
+                a = displayAnswer.textContent;
+            opr = e.key;
+            displayCurrent.textContent = "0";
+            operator = false;
+        }
+    }
+})
 
